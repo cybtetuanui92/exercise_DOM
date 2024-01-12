b6ecc6d744c0bafd717413f7cc9503fc71d8b8f3
cybtetuanui92 <mateau.tetuanui@gmail.com>
2024-01-12 09:19:10 +0100

    second commit

---
+++ exo2/script.js
@@ -0,0 +1,6 @@
+let div = document.getElementById('uniqueId')
+div.textContent = "Service des sports du Vatican, Bonjour !"
+
+div.style.color = 'red';
+div.style.fontSize = '50px'
+// console.log(div);  
